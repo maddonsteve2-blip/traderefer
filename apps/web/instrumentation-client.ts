2574bbe6ae53534546659936e91ef9f96bdb8100
stevejford1 <stevejford1@gmail.com>
2026-02-28 01:18:25 +1100

    Disable BrowserTracing integration to fix replaceState loop

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -3,4 +3,6 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://b2b50766c882354af956b84736e50f60@o4508132350033920.ingest.us.sentry.io/4508132351410176",
   tracesSampleRate: 0,
+  integrations: (defaults) =>
+    defaults.filter((i) => i.name !== "BrowserTracing"),
 });
