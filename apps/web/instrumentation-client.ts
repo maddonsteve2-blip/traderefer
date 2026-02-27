31481749d1425e6b6e7057d549efe6182ebcde61
stevejford1 <stevejford1@gmail.com>
2026-02-28 01:12:10 +1100

    Disable Sentry tracing to fix navigation replaceState loop

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -2,10 +2,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://b2b50766c882354af956b84736e50f60@o4508132350033920.ingest.us.sentry.io/4508132351410176",
-
-  // Adds request headers and IP for users
-  sendDefaultPii: true,
-
-  // Performance monitoring
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0,
 });
