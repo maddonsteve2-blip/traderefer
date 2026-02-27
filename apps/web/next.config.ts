6b86f9107fe990b563ffb4fb7f81c4b705be3aed
stevejford1 <stevejford1@gmail.com>
2026-02-28 00:42:26 +1100

    Remove Sentry wrapper from next.config.ts to fix build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   images: {
@@ -16,17 +15,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-// Make sure adding Sentry options is the last code to modify `nextConfig`
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/nextjs#options
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors
-  automaticVercelMonitors: true,
-});
+export default nextConfig;
